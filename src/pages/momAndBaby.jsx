c77e02c2fc4ea2c6d83a35591dc35f31af84131a
MohamedH1000 <122827442+MohamedH1000@users.noreply.github.com>
2023-11-13 18:44:55 +0200

    Delete src directory

--- src/pages/momAndBaby.jsx
+++
@@ -1,9 +0,0 @@
-import PopularMom from "../components/popular/popular_mom";
-
-const MomAndBaby = () => {
-    return ( 
-        <div style={{backgroundColor:'var(--bg-color-light)'}}><PopularMom/></div>
-     );
-}
- 
-export default MomAndBaby;
