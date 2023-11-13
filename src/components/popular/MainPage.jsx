7c6b3daf34e5a6038cb9464bfba90047b8dc3691
MohamedH1000 <mohammedhisham115@gmail.com>
2023-11-13 19:06:51 +0200

    all represents the popular components

---
+++ src/components/popular/MainPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+// the main page design of the website
+const PopularMainPage = () => {
+  return (
+    <div>PopularMainPage</div>
+  )
+}
+
+export default PopularMainPage
