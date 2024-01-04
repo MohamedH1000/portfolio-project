6df6420d04010f91f076cb84d6f575f0382e35ad
MohamedH1000 <122827442+MohamedH1000@users.noreply.github.com>
2024-01-04 21:13:35 +0200

    Delete src directory

--- src/components/popular/MainPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-// the main page design of the website
-const PopularMainPage = () => {
-  return (
-    <div>PopularMainPage</div>
-  )
-}
-
-export default PopularMainPage
