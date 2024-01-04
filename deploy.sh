a5633ecc2f778f346b23b97e948b55e3e2c0f43b
MohamedH1000 <122827442+MohamedH1000@users.noreply.github.com>
2024-01-04 22:26:04 +0200

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-echo "building app ..."
-npm run build
-
-echo "deploying files to the server ..."
-scp -i ~/.ssh/school -r build/* root@54.237.7.47:/var/www/softwaremech.tech
-scp -i ~/.ssh/school -r build/* root@52.86.175.15:/var/www/softwaremech.tech
-
-echo "done"
